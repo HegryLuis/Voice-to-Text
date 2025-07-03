700c133adea679dab18f9a893648d3cfac496832
HegryLuis <hegry.luis@gmail.com>
2025-07-03 17:42:05 +0300

    Changed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/api/webhook(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/api/webhook/stripe"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isPublicRoute(req)) {
@@ -10,22 +10,16 @@ export default clerkMiddleware(async (auth, req) => {
 
   const { userId } = await auth();
 
-  if (userId) {
-    return NextResponse.next();
+  if (!userId) {
+    const signInUrl = new URL("/sign-in", req.url);
+
+    signInUrl.searchParams.set("redirect_url", req.nextUrl.pathname);
+    return NextResponse.redirect(signInUrl);
   }
 
-  const signInUrl = new URL("/", req.url);
-
-  signInUrl.searchParams.set("redirect_url", req.url);
-
-  return NextResponse.redirect(signInUrl);
+  return NextResponse.next();
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
